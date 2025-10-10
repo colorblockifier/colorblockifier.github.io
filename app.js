450588dfb78d01fd10bca7fe9110cd60eca03425
comfysquirrel <jdh11235@gmail.com>
2025-10-10 06:00:33 -0500

    Import zip.js from CDN

---
+++ app.js
@@ -0,0 +1,7 @@
+import {
+    BlobReader
+} from "https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.8.7/+esm"
+
+
+const fileInput = document.querySelector("#file-input");
+// const zipFileReader = new BlobReader();
