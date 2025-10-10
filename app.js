fbcbdda259002222326f730b0f2e23bf22741e3d
comfysquirrel <jdh11235@gmail.com>
2025-10-10 08:53:04 -0500

    List textures, blocks, number, close #4

--- app.js
+++ app.js
@@ -30,18 +30,26 @@ async function loadFiles() {
     fileSizeString = `${Math.floor(reader.reader.size / 1e+6)} MB`;
     entriesAll = await reader.getEntries();
     
-    entriesFiltered = await entriesAll.filter(filter);
-    console.log(entriesFiltered);
+    entriesFiltered = entriesAll.filter(filterTextures);
+    let totalTextures = entriesFiltered.length;
+    entriesFiltered = entriesFiltered.filter(filterBlocks);
+    let totalBlocks = entriesFiltered.length;
     
-    fileSizeElement.innerText = `JAR size: ${fileSizeString}`;
-
     let filteredBlocksText = entriesFiltered.reduce((accumulator, currentValue) => {
         return `${accumulator}${currentValue.filename}\n`;
     }, "").trim();
     blockListElement.value = filteredBlocksText;
+
+    fileSizeElement.innerText = `JAR size: ${fileSizeString}, Textures: ${totalTextures}, Blocks: ${totalBlocks}`;
 }
 
-function filter(file) {
+function filterTextures(file) {
+    if (!file.filename.startsWith("assets/minecraft/textures/")) return false;
+    if (!file.filename.endsWith(".png")) return false;
+    return true;
+}
+
+function filterBlocks(file) {
     if (!file.filename.startsWith("assets/minecraft/textures/block/")) return false;
     return true;
 }
