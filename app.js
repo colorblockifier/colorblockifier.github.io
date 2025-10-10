0f3a8a11a5d916d0378226efc6abf1cb565731d8
comfysquirrel <jdh11235@gmail.com>
2025-10-10 06:09:06 -0500

    Read zip file

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 import {
-    BlobReader
-} from "https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.8.7/+esm"
-
+    ZipReader,
+    BlobReader,
+} from "https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.8.7/+esm";
 
 const fileInput = document.querySelector("#file-input");
-// const zipFileReader = new BlobReader();
+fileInput.onchange = selectFile;
+
+let entries;
+let selectedFile;
+
+async function selectFile() {
+    try {
+        selectedFile = fileInput.files[0];
+        await loadFiles();
+    } catch (error) {
+        alert(error);
+    }
+}
+
+async function loadFiles() {
+    entries = await new ZipReader(new BlobReader(selectedFile)).getEntries();
+    console.log(entries);
+}
