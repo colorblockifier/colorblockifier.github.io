0a651b109a421e19c2be5ead17fd425e7f13d848
comfysquirrel <jdh11235@gmail.com>
2025-10-14 00:05:48 -0500

    Close #26

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+</script>
+
+<Navbar>
+	<NavBrand>
+		<span class="text-xl font-semibold">colorblockifier</span>
+	</NavBrand>
+</Navbar>
