f59b77b2b9d9c417de87ae23b06bbbafecfa8c66
comfysquirrel <jdh11235@gmail.com>
2025-10-14 00:56:28 -0500

    Close #20

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,10 @@
 <Navbar>
 	<NavHamburger />
 	<NavBrand>
-		<span class="text-xl font-semibold tracking-widest">colorblockifier</span>
+		<span
+			class="bg-gradient-to-r from-red-400 via-green-400 to-purple-400 bg-clip-text font-mono text-xl font-bold tracking-widest text-transparent"
+			>colorblockifier</span
+		>
 	</NavBrand>
 	<NavUl>
 		<NavLi href="https://github.com/colorblockifier/colorblockifier.github.io">About</NavLi>
