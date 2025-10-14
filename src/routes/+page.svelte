cac0625cfe8df27f2b8e20c15a0bf6a035bf11d3
comfysquirrel <jdh11235@gmail.com>
2025-10-14 02:00:23 -0500

    Close #30

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, DarkMode } from 'flowbite-svelte';
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		DarkMode,
+		Card,
+		Fileupload,
+		Helper,
+		Label
+	} from 'flowbite-svelte';
 </script>
 
-<Navbar>
+<Navbar class="bg-gray-100">
 	<NavHamburger />
 	<NavBrand>
 		<span
@@ -10,8 +21,23 @@
 			>colorblockifier</span
 		>
 	</NavBrand>
-	<NavUl>
+	<NavUl class="hover:underline">
 		<NavLi href="https://github.com/colorblockifier/colorblockifier.github.io">About</NavLi>
 	</NavUl>
 	<DarkMode class="cursor-pointer" />
 </Navbar>
+
+<div class="flex flex-col items-center p-4">
+	<Card class="flex gap-2 p-4">
+		<Label for="jarfile">Select a Minecraft JAR to extract textures</Label>
+		<Fileupload id="jarfile" accept=".jar"></Fileupload>
+		<Helper>
+			<code>.minecraft/versions/&lt;version&gt;/&ltversion&gt;.jar</code>
+		</Helper>
+		<Helper>
+			<a class="underline" href="https://minecraft.wiki/w/.minecraft#Locating"
+				>How to find the <code>.minecraft</code> folder</a
+			>
+		</Helper>
+	</Card>
+</div>
