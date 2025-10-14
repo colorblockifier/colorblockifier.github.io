4e685ddf8d04f5ead1627f1950a8be3c73c3f984
comfysquirrel <jdh11235@gmail.com>
2025-10-13 22:40:35 -0500

    Try to fix pages

--- svelte.config.js
+++ svelte.config.js
@@ -9,13 +9,10 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: 'app.html',
+			fallback: undefined,
 			precompress: false,
 			strict: true
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		})
 	},
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
